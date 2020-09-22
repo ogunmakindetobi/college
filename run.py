d0fc90bb28c9e49fbfdf9cf23c45ae3673e6f7ad
ogunmakindetobi <ogunmakindetobi@outlook.com>
2020-09-22 20:08:46 +0000

    new files

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
+    
 app = Flask(__name__)
+app.secret_key = 'SECRET_KEY'
 
 
 @app.route('/')
@@ -10,15 +14,19 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title='About')
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET","POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+              request.form["name"]))
+
+    return render_template("contact.html", page_title='Contact')
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title='Careers')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
